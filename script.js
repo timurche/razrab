bc37a334f6dfce37a4964e6ab78d37559aa64698
timurche <timurnamail@mail.ru>
2020-04-18 17:14:35 +0500

    это начало. Задание 1

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var money = prompt("Ваш бюджет на месяц?");
+var time = new Date(prompt("Введите дату в формате YYYY-MM-DD"));
+console.log(money, time)
+var firstQuest = prompt("Введите обязательную статью расходов в этом месяце");
+var secondQuest = prompt("Во сколько обойдется?");
+var firstQuest1 = prompt("Введите обязательную статью расходов в этом месяце");
+var secondQuest1 = prompt("Во сколько обойдется?");
+var appData = {
+    money, 
+    timeData: time,
+    expenses: {
+        [firstQuest]: secondQuest,
+        [firstQuest1]: secondQuest1
+    },
+    optionalExpenses:{},
+    income: [],
+    savings: false,
+
+}
+console.log(appData.expenses);
+alert("ваш бджет = " + money/30)
