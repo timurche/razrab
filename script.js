70a3a75c51ab4c34c6ad92f73b09d9950d3eab9d
timurche <timurnamail@mail.ru>
2020-04-26 12:54:59 +0500

    3 цикла + мнение эксперта

--- script.js
+++ script.js
@@ -13,21 +13,59 @@ var appData = {
 
 }
 
-for (let i = 0; i < 2; i++) {
+// for (let i = 0; i < 2; i++) {
+//     var firstQuest = prompt("Введите обязательную статью расходов в этом месяце");
+//     var secondQuest = prompt("Во сколько обойдется?");
+    
+//     if ((typeof(firstQuest)) === 'string' && (typeof(firstQuest)) != null && (typeof(secondQuest)) != null
+//     && secondQuest != '' && firstQuest != '' && firstQuest.length < 50 && firstQuest != appData.expenses[firstQuest]) {
+//             console.log('Correct')
+//             appData.expenses[firstQuest] = secondQuest;
+//         } else {
+//             alert('Не хами мне');
+//             --i;
+
+//         }
+    
+// }
+
+// let i=0;
+// while (i < 2) {
+//     var firstQuest = prompt("Введите обязательную статью расходов в этом месяце");
+//     var secondQuest = prompt("Во сколько обойдется?");
+    
+//     if ((typeof(firstQuest)) === 'string' && (typeof(firstQuest)) != null && (typeof(secondQuest)) != null
+//         && secondQuest != '' && firstQuest != '' && firstQuest.length < 50 && firstQuest != appData.expenses[firstQuest]) {
+//             console.log('Correct')
+//             appData.expenses[firstQuest] = secondQuest;
+//         } else {
+//             alert('Не хами мне');
+//             --i;
+
+//         }
+//     i++
+// }
+
+let i=0;
+do {
     var firstQuest = prompt("Введите обязательную статью расходов в этом месяце");
     var secondQuest = prompt("Во сколько обойдется?");
-    
+        
     if ((typeof(firstQuest)) === 'string' && (typeof(firstQuest)) != null && (typeof(secondQuest)) != null
-        && secondQuest != '' && firstQuest != '' && firstQuest.length < 50) {
-            console.log('Correct')
-            appData.expenses[firstQuest] = secondQuest;
+    && secondQuest != '' && firstQuest != '' && firstQuest.length < 50 
+    && firstQuest != appData.expenses[firstQuest]) {
+        console.log('Correct')
+        appData.expenses[firstQuest] = secondQuest;
         } else {
             alert('Не хами мне');
-            --i;
-
+            i--;
         }
-
-    
-    
+        i++
+}while (i < 2)
+if ((appData.budget / 30) <= 1000) {
+    alert(appData.budget / 30 + " - это нищебродский бюджет, извини.\n")
+} else if ((appData.budget / 30) > 1000  && (appData.budget / 30) <= 10000) {
+    alert(appData.budget / 30 + " - этот бюджет средненький, так-то.\n")
+} else if ((appData.budget / 30) > 10000){
+    alert(appData.budget / 30 + " - это нормальный бюджет, красава.\n")
 }
-alert("ваш бюджет = " + money / 30);
