17c6563f33f1f9b95860a0ced517b091f86a1a22
timurche <timurnamail@mail.ru>
2020-05-02 04:27:15 +0500

    опциональные расходы

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 'use strict';
+var money, time;
 
-var money = +prompt("Ваш бюджет на месяц?");
-var time = new Date(prompt("Введите дату в формате YYYY-MM-DD"));
+function start(){
+    let err = '';
+    do {money = +prompt(err + "Ваш бюджет на месяц?");
+        err = 'Ошибка! '; 
+    }while(isNaN(money) || money == null || money == "");
+    time = new Date(prompt("Введите дату в формате YYYY-MM-DD"));
+}
+//start();
 
 var appData = {
     budget: money, 
@@ -13,59 +20,51 @@ var appData = {
 
 }
 
-// for (let i = 0; i < 2; i++) {
-//     var firstQuest = prompt("Введите обязательную статью расходов в этом месяце");
-//     var secondQuest = prompt("Во сколько обойдется?");
-    
-//     if ((typeof(firstQuest)) === 'string' && (typeof(firstQuest)) != null && (typeof(secondQuest)) != null
-//     && secondQuest != '' && firstQuest != '' && firstQuest.length < 50 && firstQuest != appData.expenses[firstQuest]) {
-//             console.log('Correct')
-//             appData.expenses[firstQuest] = secondQuest;
-//         } else {
-//             alert('Не хами мне');
-//             --i;
-
-//         }
-    
-// }
-
-// let i=0;
-// while (i < 2) {
-//     var firstQuest = prompt("Введите обязательную статью расходов в этом месяце");
-//     var secondQuest = prompt("Во сколько обойдется?");
-    
-//     if ((typeof(firstQuest)) === 'string' && (typeof(firstQuest)) != null && (typeof(secondQuest)) != null
-//         && secondQuest != '' && firstQuest != '' && firstQuest.length < 50 && firstQuest != appData.expenses[firstQuest]) {
-//             console.log('Correct')
-//             appData.expenses[firstQuest] = secondQuest;
-//         } else {
-//             alert('Не хами мне');
-//             --i;
-
-//         }
-//     i++
-// }
-
-let i=0;
-do {
-    var firstQuest = prompt("Введите обязательную статью расходов в этом месяце");
-    var secondQuest = prompt("Во сколько обойдется?");
+function chooseExpenses(){
+    for (let i = 0; i < 2; i++) {
+        var firstQuest = prompt("Введите обязательную статью расходов в этом месяце");
+        var secondQuest = prompt("Во сколько обойдется?");
         
-    if ((typeof(firstQuest)) === 'string' && (typeof(firstQuest)) != null && (typeof(secondQuest)) != null
-    && secondQuest != '' && firstQuest != '' && firstQuest.length < 50 
-    && firstQuest != appData.expenses[firstQuest]) {
-        console.log('Correct')
-        appData.expenses[firstQuest] = secondQuest;
-        } else {
-            alert('Не хами мне');
-            i--;
-        }
-        i++
-}while (i < 2)
-if ((appData.budget / 30) <= 1000) {
-    alert(appData.budget / 30 + " - это нищебродский бюджет, извини.\n")
-} else if ((appData.budget / 30) > 1000  && (appData.budget / 30) <= 10000) {
-    alert(appData.budget / 30 + " - этот бюджет средненький, так-то.\n")
-} else if ((appData.budget / 30) > 10000){
-    alert(appData.budget / 30 + " - это нормальный бюджет, красава.\n")
+        if ((typeof(firstQuest)) === 'string' && (typeof(firstQuest)) != null && (typeof(secondQuest)) != null
+        && secondQuest != '' && firstQuest != '' && firstQuest.length < 50 && firstQuest != appData.expenses[firstQuest]) {
+                console.log('Correct')
+                appData.expenses[firstQuest] = secondQuest;
+        }   else {
+                alert('Не хами мне');
+                --i;
+
+            }
+        
+    }
 }
+//chooseExpenses()
+
+function detectLevel(){
+    if ((appData.budget / 30) <= 1000) {
+        alert(appData.budget / 30 + " - это нищебродский бюджет, извини.\n")
+    } else if ((appData.budget / 30) > 1000  && (appData.budget / 30) <= 10000) {
+        alert(appData.budget / 30 + " - этот бюджет средненький, так-то.\n")
+    } else if ((appData.budget / 30) > 10000){
+        alert(appData.budget / 30 + " - это нормальный бюджет, красава.\n")
+    }
+}
+//detectLevel()
+
+function chooseOptExpenses(){
+    for (let i = 1; i < 4; i++) {
+        var quest = prompt(`Статья необязательных расходов № ${i}: `);
+        
+        if ((typeof(quest)) === 'string' && (typeof(quest)) != null && (typeof(quest)) != null
+        && quest != '' && quest != '' && quest.length < 50) {
+                console.log('Correct')
+                appData.optionalExpenses[i] = quest;
+        }   else {
+                alert('Не хами мне');
+                --i;
+
+            }
+        
+    }
+console.log(appData.optionalExpenses);
+}
+chooseOptExpenses();
